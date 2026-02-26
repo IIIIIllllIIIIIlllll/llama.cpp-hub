2e6f4ea755ae16cb249f256ea00f50834acd5b7b
IIIIIllllIIIIIlllll <2432896620@qq.com>
2026-02-26 17:59:33 +0800

    默认添加参数：--host 0.0.0.0

--- src/main/java/org/mark/llamacpp/server/LlamaServerManager.java
+++ src/main/java/org/mark/llamacpp/server/LlamaServerManager.java
@@ -1238,6 +1238,8 @@ public class LlamaServerManager {
 		sb.append(" --alias ").append(targetModel.getModelId());
 		
 		sb.append(" --timeout 36000");
+		// 允许任意IP地址访问
+		sb.append(" --host 0.0.0.0");
 		
 		return sb.toString().trim();
 	}
