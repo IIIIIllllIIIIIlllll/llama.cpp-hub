8e4e007497f74e4a3d542dc1e39a77921d2c10db
IIIIIllllIIIIIlllll <2432896620@qq.com>
2026-07-08 23:11:56 +0800

    增加：mimo的图标判定

--- src/main/resources/web/js/model-icon.js
+++ src/main/resources/web/js/model-icon.js
@@ -1,26 +1,27 @@
 function getModelIcon(architecture) {
     const archName = (architecture || '').toLowerCase().replace(/[^a-z]/g, '');
     const iconMap = {
-        'qwen': 'icon/qwen.png',
-        'glm': 'icon/glm.png',
-        'hunyuan': 'icon/hunyuan.png',
-        'mistral': 'icon/mistral3.png',
-        'gpt': 'icon/openai.png',
-        'seed': 'icon/seed_oss.png',
-        'llama': 'icon/llama.png',
-        'kimi': 'icon/kimi.png',
-        'minimax': 'icon/minimax.png',
-        'gemma': 'icon/gemma.png',
+		'qwen': 'icon/qwen.png',
+		'glm': 'icon/glm.png',
+		'hunyuan': 'icon/hunyuan.png',
+		'mistral': 'icon/mistral3.png',
+		'gpt': 'icon/openai.png',
+		'seed': 'icon/seed_oss.png',
+		'llama': 'icon/llama.png',
+		'kimi': 'icon/kimi.png',
+		'minimax': 'icon/minimax.png',
+		'gemma': 'icon/gemma.png',
 		'deepseek': 'icon/deepseek.png',
 		'deepseek2': 'icon/deepseek.png',
 		'deepseek3': 'icon/deepseek.png',
-        'step': 'icon/step35.png',
-        'paddleocr': 'icon/paddleocr.png',
+		'step': 'icon/step35.png',
+		'paddleocr': 'icon/paddleocr.png',
 		'bailingmoe': 'icon/bailingmoe.png',
 		'lfm': 'icon/lfm.png',
 		'lfm2': 'icon/lfm.png', 
 		'nemotron': 'icon/nemotron.png',
-		'bert': 'icon/bert.png'
+		'bert': 'icon/bert.png',
+		'mimo': 'icon/mimo.png',
     };
     for (const [key, icon] of Object.entries(iconMap)) {
         if (archName.includes(key)) return icon;
